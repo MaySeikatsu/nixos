0e4dbfc623a841fc6a4541226a5d1ab8ed9b66c0
maike <maynoshinseikatsu@gmail.com>
2025-03-25 00:18:29 +0100

    feat: vencord theme enabled

--- modules/home-manager/nixcord.nix
+++ modules/home-manager/nixcord.nix
@@ -6,42 +6,38 @@
 {
   # programs.nixcord.enable = true;
     programs.nixcord = {
-    enable = true;
-    discord = {
       enable = true;
-      package = pkgs.discord;
-      # package = pkgs.vesktop;
-      vencord = {
-        enable = true;
-        unstable = true;
-      };
-    };
       vesktop = {
         enable = true;
       };   
-
-  #   enable = true;  # enable Nixcord. Also installs discord package
-  #
-  #   quickCss = "some CSS";  # quickCSS file
-  #   config = {
-  #     useQuickCss = true;   # use out quickCSS
-  #     themeLinks = [        # or use an online theme
-  #       "https://raw.githubusercontent.com/link/to/some/theme.css"
-  #     ];
-  #     frameless = true; # set some Vencord options
-  #     plugins = {
-  #       hideAttachments.enable = true;    # Enable a Vencord plugin
-  #       ignoreActivities = {    # Enable a plugin and set some options
-  #         enable = true;
-  #         ignorePlaying = true;
-  #         ignoreWatching = true;
-  #         ignoredActivities = [ "someActivity" ];
-  #       };
-  #     };
-  #   };
-  #   extraConfig = {
-  #     # Some extra JSON config here
-  #     # ...
-  #   };
+      discord = {
+        enable = true;
+        package = pkgs.discord;
+        vencord = {
+          enable = true;
+          unstable = true;
+        };
+      };
+      # quickCss = "some CSS";  # quickCSS file
+      config = {
+        useQuickCss = true;   # use out quickCSS
+        themeLinks = [        # or use an online theme
+          "https://refact0r.github.io/system24/theme/system24.theme.css"
+        ];
+        # frameless = true; # set some Vencord options
+        # plugins = {
+        #   hideAttachments.enable = true;    # Enable a Vencord plugin
+        #   ignoreActivities = {    # Enable a plugin and set some options
+        #     enable = true;
+        #     ignorePlaying = true;
+        #     ignoreWatching = true;
+        #     ignoredActivities = [ "someActivity" ];
+        #   };
+        # };
+      };
+      extraConfig = {
+        # Some extra JSON config here
+        # ...
+      };
     };
 }
