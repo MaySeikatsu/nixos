28609c9dc939edb0fb4e69f9daca5e9e49234a53
maike-pc <maynoshinseikatsu@gmail.com>
2025-05-10 20:44:40 +0200

    fix: setting workspace 1 to workspace zero, as fix did not work

--- modules/home-manager/hyprland/workspace.nix
+++ modules/home-manager/hyprland/workspace.nix
@@ -2,7 +2,7 @@
 {
   wayland.windowManager.hyprland.settings = {
       workspace = [
-        "name:1,monitor:DP-2" # start workspace one on main monitor (in this case dp-2) to start steam games correctly
+        "name:0,monitor:DP-2" # start workspace one on main monitor (in this case dp-2) to start steam games correctly
       ];
   };
 }
