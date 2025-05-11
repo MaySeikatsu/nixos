a39594c83c716906056e943ea210687c1d2701e4
maike <maynoshinseikatsu@gmail.com>
2025-05-11 19:55:47 +0200

    wip: adding matugen themes for ghostty and yazi

--- modules/home-manager/matugen.nix
+++ modules/home-manager/matugen.nix
@@ -8,13 +8,24 @@
     matugen = {
       enable = true;
       variant = "dark";
+      wallpaper = ./../../../../Downloads/thumb-1920-1377800.png;
+
+      templates = {
+        ghostty = {
+            input_path = "./matugen_templates/ghostty.theme";
+            output_path = "/home/maike/.config/ghostty/themes/matugen.theme"; #output path needs to be created manually if not already existing
+          };
+          yazi = {
+            input_path = "./matugen_templates/yazi.toml";
+            output_path = "/home/maike/.config/yazi/colors.toml";
+          };
       # palette = "default";
       
       # settings = {
       #   # config = {
       #   #   reload_apps = true;
       #   # };
-      # };
+      };
     };
   };
 }
