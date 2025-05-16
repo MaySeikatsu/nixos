b2152906ce89fecb689c7db8b122867c1a69e22c
maike <maynoshinseikatsu@gmail.com>
2025-05-16 11:06:33 +0200

    feat: nvidia driver working with optimus now

--- hosts/nixos-legion/configuration.nix
+++ hosts/nixos-legion/configuration.nix
@@ -98,11 +98,11 @@
     # Enable this if you have graphical corruption issues or application crashes after waking
     # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
     # of just the bare essentials.
-    powerManagement.enable = false;
+    powerManagement.enable = true; # not really needed but battery left time was shown higher even though it didn't change gpu behavior
 
     # Fine-grained power management. Turns off GPU when not in use.
     # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = true;
+    powerManagement.finegrained = true; # Important for battery life
     # powerManagement.finegrained = false;
 
     # Use the NVidia open source kernel module (not to be confused with the
