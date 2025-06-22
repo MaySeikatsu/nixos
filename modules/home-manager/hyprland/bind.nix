31ae0c8934595e63a3f9e91868e1f1f24e71ca91
maike <maynoshinseikatsu@gmail.com>
2025-06-22 23:49:03 +0200

    feat: edit power profile, add wallust refresh bind for hyprland

--- modules/home-manager/hyprland/bind.nix
+++ modules/home-manager/hyprland/bind.nix
@@ -8,7 +8,7 @@
       # Wallust Commands:
       "$mainMod+Shift, m, exec, ~/.config/nixos/scripts/wall_cycle.sh"
       "$mainMod+Shift, n, exec, ~/config/nixos/scripts/wall_random.sh"
-      "$mainMod+Shift, r, exec, wallrust run ~/.current-wallpaper"
+      "$mainMod+Shift, r, exec, ~/config/nixos/scripts/wallust_refresh_current_wp.sh"
 
       # Window/Session actions
       "$mainMod, Q, killactive,"
---
+++ scripts/wallust_refresh_current_wp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wallust run ~/.current-wallpaper
