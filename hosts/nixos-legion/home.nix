1e0aabeec22a03816a253a86b42403a3d49d2cdc
maike <maynoshinseikatsu@gmail.com>
2025-06-22 20:14:08 +0200

    feat: nvidia boot profile added

--- hosts/nixos-legion/home.nix
+++ hosts/nixos-legion/home.nix
@@ -7,6 +7,7 @@
   wayland.windowManager.hyprland.settings = {
     env = [
           "AQ_DRM_DEVICES,/dev/dri/card2" #/dev/dri/card1"# THIS SETTING IS HARDCODED FOR THE LEGION DEVICE should also work for the pc though, but does the exact opposite (on legion uses iGPU on PC uses dGPU)
+          # "AQ_DRM_DEVICES,/dev/dri/card1" # USE THIS WHEN NVIDIA CARD IS ACTIVE TO RUN HYPRLAND ON INTERNAL GPU once its one the DEDICATED CARD BECOMES CARD1 instead of the intergrted card
     ];
   };
 }
