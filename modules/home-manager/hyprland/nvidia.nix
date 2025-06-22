7cf9d8f5beeca4c7292792c3e879d4535e0ef366
maike <maynoshinseikatsu@gmail.com>
2025-06-22 19:14:17 +0200

    feat: added quickshell enabled nvidia

--- modules/home-manager/hyprland/nvidia.nix
+++ modules/home-manager/hyprland/nvidia.nix
@@ -2,11 +2,11 @@
 {
   wayland.windowManager.hyprland.settings = {
     env = [
-    # "LIBVA_DRIVER_NAME,nvidia"
-    # "__GLX_VENDOR_LIBRARY_NAME,nvidia"
-    # "__GL_VRR_ALLOWED,1"
-    # "WLR_DRM_NO_ATOMIC,1"
-      #    # "AQ_DRM_DEVICES,/dev/dri/card2" #/dev/dri/card1"# THIS SETTING IS HARDCODED FOR THE LEGION DEVICE should also work for the pc though, but does the exact opposite (on legion uses iGPU on PC uses dGPU)
+    "LIBVA_DRIVER_NAME,nvidia"
+    "__GLX_VENDOR_LIBRARY_NAME,nvidia"
+    "__GL_VRR_ALLOWED,1"
+    "WLR_DRM_NO_ATOMIC,1"
+         # "AQ_DRM_DEVICES,/dev/dri/card2" #/dev/dri/card1"# THIS SETTING IS HARDCODED FOR THE LEGION DEVICE should also work for the pc though, but does the exact opposite (on legion uses iGPU on PC uses dGPU)
   ];
     cursor = {
       no_hardware_cursors = true;
