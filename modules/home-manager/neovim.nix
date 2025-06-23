11450bb3da318a708dde71986f69bf335746ebd5
maike <maynoshinseikatsu@gmail.com>
2025-06-23 09:08:47 +0200

    feat: added wallust support for lazyvim with auto plugin create

--- modules/home-manager/neovim.nix
+++ modules/home-manager/neovim.nix
@@ -6,6 +6,9 @@
   #   inputs.nixCats-nvim.homeModule
   # ];
 
+  # Creates a wallust.lua file inside of ~/.config/wallust or overwrites it with the given values of the file included in the repo 
+  # xdg.configFile."./nvim/lua/plugins/wallust.lua".source = ../../../ressources/dos/nvim/lua/plugins/wallust.lua; #For neovim config to work with wallust
+
   programs.neovim = {
     enable = true;
     # Add extra packages as needed
---
+++ ressources/dots/nvim/lua/plugins/wallust.lua
@@ -0,0 +1,47 @@
+return
+{
+    {
+    "RedsXDD/neopywal.nvim",
+    name = "neopywal",
+    lazy = false,
+    priority = 1000,
+    opts = {
+        use_wallust = true,
+        transparent_background = true,
+        -- colorscheme_file = "~/.cache/wallust/colors_neopywal.vim",
+        -- use_palette = "~/.cache/wallust/colors_neopywal.vim",
+
+    -- custom_colors = function(C)
+    --     return {
+    --         -- background = C.none,
+    --         background = C.background,
+    --         foreground = C.foreground,
+    --         cursor = C.cursor,
+    --         color0 = C.color0,
+    --         color1 = C.color1,
+    --         color2 = C.color2,
+    --         color3 = C.color3,
+    --         color4 = C.color4,
+    --         color5 = C.color5,
+    --         color6 = C.color6,
+    --         color7 = C.color7,
+    --         color8 = C.color8,
+    --         color9 = C.color9,
+    --         color10 = C.color10,
+    --         color11 = C.color11,
+    --         color12 = C.color12,
+    --         color13 = C.color13,
+    --         color14 = C.color14,
+    --         color15 = C.color15,
+    --     }
+    -- end,
+    --
+      },
+    },
+    -- {
+      -- "LazyVim/LazyVim", 
+      -- opts = {
+      --   colorscheme = "neopywal"
+      -- }
+    -- }
+}
