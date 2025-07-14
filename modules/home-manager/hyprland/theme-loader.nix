ab62073d9f55b94d04b06f11e97717465a25a847
maike-pc <maynoshinseikatsu@gmail.com>
2025-07-14 16:49:36 +0200

    feat: added caelestial quickshell config

--- modules/home-manager/hyprland/theme-loader.nix
+++ modules/home-manager/hyprland/theme-loader.nix
@@ -3,7 +3,8 @@
   imports = [
     # ./theme/theme-default.nix
     # ./theme/theme-glassy-blur.nix
-    ./theme/theme-squircle-glass.nix
+    # ./theme/theme-squircle-glass.nix
+    ./theme/theme-caelestia-quickshell.nix
     # ./theme/theme-batterysaver.nix
     # ./theme/theme-no-rounding.nix
     ./theme/windowrules-default.nix
