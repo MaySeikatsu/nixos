2b79a6300253c721fe8dd134cbc25bee7f18a8c3
maike-pc <maynoshinseikatsu@gmail.com>
2025-07-24 11:37:05 +0200

    feat: adjusted ram size

--- scripts/openrbg.sh
+++ scripts/openrbg.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env sh
 
 # Adjusts openrgb devices to the current color of the wallpaper, crrently just set to device 0, add other devices if needed
-openrgb -d 0 -c $(grep '^let color2' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
+openrgb -d 0 -c $(grep '^let color1' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/') -d 1 -c $(grep '^let color1' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/') -d 2 -c $(grep '^let color1' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
+# openrgb -d 1 -c $(grep '^let color2' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
+# openrgb -d 2 -c $(grep '^let color2' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
+# openrgb -d 3 -c $(grep '^let color2' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
+# openrgb -d 4 -c $(grep '^let color2' ~/.cache/wal/colors-wal.vim | sed -E 's/.*= "#([0-9A-Fa-f]+)"/\1/')
