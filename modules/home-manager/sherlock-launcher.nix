b6aee5e384438d4026a9c0e6183cfb41edbecb0c
maike <maynoshinseikatsu@gmail.com>
2025-08-04 17:40:50 +0200

    fix: adjusted sherlock settings for better response time

--- modules/home-manager/sherlock-launcher.nix
+++ modules/home-manager/sherlock-launcher.nix
@@ -1,5 +1,5 @@
-{inputs, pkgs, ...}:{
-  
+{ inputs, pkgs, ... }: {
+
   # use this if not already defined in flake
   # imports = [
   #   inputs.sherlock.homeManagerModules.default
@@ -8,52 +8,50 @@
   programs.sherlock = {
     enable = true;
     settings = {
-      aliases = {
-        vesktop = {
-          name = "Discord";
-        };
-      };
-      launchers = [
-        {
-          name = "App Launcher";
-          alias = "app";
-          type = "app_launcher";
-          args = {};
-          priority = 1;
-          home = true;
-        }
-        # {
-        #   name = "Web Search";
-        #   display_name = "Web Search";
-        #   tag_start = "{keyword}";
-        #   tag_end = "{keyword}";
-        #   alias = "gg";
-        #   type = "web_launcher";
-        #   args = {
-        #       "search_engine" = "ecosia";
-        #       "icon" = "ecosia";
-        #   };
-        #   priority = 10;
-        # }
-      ];
+      aliases = { vesktop = { name = "Discord"; }; };
+      launchers = [{
+        name = "App Launcher";
+        alias = "app";
+        type = "app_launcher";
+        args = { };
+        priority = 1;
+        home = true;
+      }
+      # {
+      #   name = "Web Search";
+      #   display_name = "Web Search";
+      #   tag_start = "{keyword}";
+      #   tag_end = "{keyword}";
+      #   alias = "gg";
+      #   type = "web_launcher";
+      #   args = {
+      #       "search_engine" = "ecosia";
+      #       "icon" = "ecosia";
+      #   };
+      #   priority = 10;
+      # }
+        ];
       config = {
         # appearance = {
         #   icon_size = 22;
         #   opacity = 0.9;
         #   # gsk_renderer = "ngl"; #default is cairo
         # };
-        # behavior = {
-        #   daemonize = true;
-        # };
+        behavior = {
+          daemonize = false;
+          caching = true;
+          cache = "~/.cache/sherlock/sherlock_desktop_cache.json";
+          animate = false;
+        };
         # units = {
         #   lengths = "meter";
         # };
 
-      # style = null
+        # style = null
 
-      # debug = {
-      #     try_suppress_warnings = true;
-      #   };
+        # debug = {
+        #     try_suppress_warnings = true;
+        #   };
       };
       # ignore = ''
       #   Avahi*
