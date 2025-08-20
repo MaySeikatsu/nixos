a74899149aa8459cf67f800581e145b3c50a095b
maike <maynoshinseikatsu@gmail.com>
2025-08-20 15:20:55 +0200

    feat: template for zellij for wallust and transparency adjusted

--- modules/home-manager/hyprland/theme-loader.nix
+++ modules/home-manager/hyprland/theme-loader.nix
@@ -2,11 +2,11 @@
   imports = [
     # ./theme/theme-default.nix
     # ./theme/theme-glassy-blur.nix
-    ./theme/theme-squircle-glass.nix
-    # ./theme/theme-no-padding.nix
     # ./theme/theme-caelestia-quickshell.nix
     # ./theme/theme-batterysaver.nix
     # ./theme/theme-no-rounding.nix
+    # ./theme/theme-squircle-glass.nix
+    ./theme/theme-no-padding.nix
     ./theme/windowrules-default.nix
   ];
 }
--- modules/home-manager/hyprland/theme/theme-no-padding.nix
+++ modules/home-manager/hyprland/theme/theme-no-padding.nix
@@ -1,24 +1,22 @@
-{...}:
-{
+{ ... }: {
   wayland.windowManager.hyprland.settings = {
     exec = [
-    #   "exec = gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle-dracula'"
-    #   "exec = gsettings set org.gnome.desktop.interface gtk-theme 'rose-pine-moon'"
+      #   "exec = gsettings set org.gnome.desktop.interface icon-theme 'Tela-circle-dracula'"
+      #   "exec = gsettings set org.gnome.desktop.interface gtk-theme 'rose-pine-moon'"
       "exec = gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'"
     ];
 
     general = {
       gaps_in = 0;
       gaps_out = 0;
-      border_size = 2;
+      # border_size = 2;
+      border_size = 0;
       # "col.active_border" = "rgba(ca9ee6ff) rgba(f2d5cfff) 45deg";
       # "col.inactive_border" = "rgba(b4befecc) rgba(6c7086cc) 45deg";
       layout = "dwindle";
       resize_on_border = true;
       # allow_tearing = true;
-      snap = {
-        enabled = true;
-      };
+      snap = { enabled = true; };
     };
 
     # Colors are commented out for stylix theming
@@ -31,11 +29,11 @@
 
     decoration = {
       rounding = 0;
-      rounding_power = 0.0; #4.0 is a squircle
+      rounding_power = 0.0; # 4.0 is a squircle
       active_opacity = 1.0;
       inactive_opacity = 0.96;
-      dim_special = 0.20;
-      dim_around = 0.60;
+      dim_special = 0.2;
+      dim_around = 0.6;
       border_part_of_window = true;
 
       blur = {
@@ -46,7 +44,7 @@
         passes = 6;
         xray = true;
 
-        noise = 0.0117;
+        noise = 1.17e-2;
         vibrancy = 0.7696;
         # vibrancy_darkness = 0.9;
         contrast = 0.97;
