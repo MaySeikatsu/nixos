e0298df25264fe666efe7d58b3026966ca0ad2a7
Maike <>
2026-04-21 17:21:56 +0200

    fix: screenshare remove desktop portals

--- hosts/configuration-shared.nix
+++ hosts/configuration-shared.nix
@@ -152,11 +152,14 @@ in {
     xdgOpenUsePortal = true;
     extraPortals = [
       pkgs.xdg-desktop-portal-wlr
-      pkgs.xdg-desktop-portal-gtk
-      pkgs.xdg-desktop-portal-gnome
+      # pkgs.xdg-desktop-portal-gtk
+      # pkgs.xdg-desktop-portal-gnome
       # pkgs.xdg-desktop-portal-hyprland
       # pkgs.xdg-desktop-portal-kde
       pkgs.gnome-keyring
+    ];
+    configPackages = [
+      pkgs.xdg-desktop-portal-wlr
     ];
   };
 
