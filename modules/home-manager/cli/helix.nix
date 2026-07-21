341c244831078fc86848778e006d71f447b7c6db
MaySeikatsu <mayseikatsu@proton.me>
2026-07-21 09:10:46 +0200

    feat(helix): add binds and config options to close the gap to lazyvim

--- modules/home-manager/cli/helix.nix
+++ modules/home-manager/cli/helix.nix
@@ -35,102 +35,59 @@
       theme = "rose_pine_moon";
       # Zellij integration: works because :sh just talks to the surrounding
       # zellij session's server. -f = floating pane, -c = close when app exits.
-      keys.normal.space = {
-        T = ":sh zellij action new-pane -f"; # Space+T floating console
-        L = ":sh zellij run -fc -- lazygit"; # Space+L floating lazygit
-        Y = ":sh zellij run -fc -- yazi"; # Space+Y floating yazi (Space+e/E = builtin explorer)
-        B = ":sh zellij run -c -d left -- broot"; # Space+B sidebar filetree
-        X = ":sh zellij plugin -f -- zellij:strider"; # Space+X floating strider tree
+      keys.normal = {
+        S-h = "goto_previous_buffer";
+        S-l = "goto_next_buffer";
+        C-r = "redo";
+        C-x = ":buffer-close";
+        C-S-x = ":buffer-close-others";
+        C-b = "buffer_picker";
+
+        space = {
+          T = ":sh zellij action new-pane -f"; # Space+T floating console
+          L = ":sh zellij run -fc -- lazygit"; # Space+L floating lazygit
+          Y = ":sh zellij run -fc -- yazi"; # Space+Y floating yazi (Space+e/E = builtin explorer)
+          B = ":sh zellij run -c -d left -- broot"; # Space+B sidebar filetree
+          X = ":sh zellij plugin -f -- zellij:strider"; # Space+X floating strider tree
+        };
       };
+
       editor = {
         evil = true;
         # clipboard-provider = "";
         auto-format = true; # global default, can be overridden per language
         line-number = "relative";
-        bufferline = "multiple";
+        bufferline = "multiple"; # Enable bufferline at top showing open/active buffers
         file-picker.hidden = false;
+        statusline = {
+          left = ["mode" "spinner" "version-control" "file-name"];
+        };
+        end-of-line-diagnostics = "hint";
+        inline-diagnostics = {
+          cursor-line = "error";
+          # other-lines = "";
+        };
+        lsp = {
+          # enable = true;
+          auto-signature-help = false;
+          display-messages = true;
+        };
         # softwrap.enable = true;
-        # lsp = {
-        #   enable = true;
-        # };
         # search = "";
+        rulers = [120];
         color-modes = true;
+        true-color = true;
         cursor-shape = {
           insert = "bar";
           normal = "block";
           select = "underline";
         };
+        # indent-guides = {
+        #   character = "╎"; # "|"
+        #   render = true;
+        # };
       };
     };
-    # languages = {
-    #   language = [
-    #     # Nix
-    #     {
-    #       name = "nix";
-    #       auto-format = true;
-    #       formatter = {
-    #         command = lib.getExe pkgs.alejandra;
-    #       };
-    #       language-servers = ["nil"];
-    #     }
-    #
-    #     # Rust
-    #     {
-    #       name = "rust";
-    #       auto-format = true;
-    #       language-servers = ["rust-analyzer"];
-    #     }
-    #
-    #     # TypeScript
-    #     {
-    #       name = "typescript";
-    #       auto-format = true;
-    #       language-servers = ["typescript-language-server"];
-    #     }
-    #     {
-    #       name = "tsx";
-    #       auto-format = true;
-    #       language-servers = ["typescript-language-server"];
-    #     }
-    #
-    #     # Terraform
-    #     {
-    #       name = "terraform";
-    #       auto-format = true;
-    #       language-servers = ["terraform-ls"];
-    #     }
-    #
-    #     # YAML
-    #     {
-    #       name = "yaml";
-    #       auto-format = true;
-    #       language-servers = ["yaml-language-server"];
-    #     }
-    #
-    #     # TOML
-    #     {
-    #       name = "toml";
-    #       auto-format = true;
-    #       language-servers = ["taplo"];
-    #       formatter = {
-    #         command = lib.getExe pkgs.taplo;
-    #         args = ["format" "-"];
-    #       };
-    #     }
-    #
-    #     # Python
-    #     {
-    #       name = "python";
-    #       auto-format = true;
-    #       language-servers = ["pyright"];
-    #       formatter = {
-    #         command = lib.getExe pkgs.black;
-    #         args = ["-"];
-    #       };
-    #     }
-    #   ];
-    # };
-
     extraPackages = with pkgs; [
       nil
       alejandra
